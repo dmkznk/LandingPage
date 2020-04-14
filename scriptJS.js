0e7f78fe8664d22c0c37042b189c218334c55137
Dima <admin@MacBook-Pro-Admin.local>
2020-04-15 01:19:36 +0300

    added 5th section with members pictures and info about

--- scriptJS.js
+++ scriptJS.js
@@ -4,3 +4,13 @@ openMenu.onclick = function(){
     menuColum.classList.toggle("active")
     document.body.classList.toggle("body-overflow")
 }
+
+const memberAbout = document.querySelectorAll('.about-members');
+const avatars = document.querySelectorAll('.team-member-avatar');
+
+avatars.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    memberAbout.forEach(elem => elem.classList.remove('active')) 
+    memberAbout[index].classList.add('active')
+  })
+})
